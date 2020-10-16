3671dfd5d082b84d44d2bd49154406b721dedf1e
Ercan Karahan <ercankarahan@hotmail.de>
2020-10-16 14:04:24 +0300

    Add project files.

---
+++ ProgresBar/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace ProgresBar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.prgBar = new System.Windows.Forms.ProgressBar();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // prgBar
+            // 
+            this.prgBar.Location = new System.Drawing.Point(129, 67);
+            this.prgBar.Name = "prgBar";
+            this.prgBar.Size = new System.Drawing.Size(530, 31);
+            this.prgBar.TabIndex = 0;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnLoad.Location = new System.Drawing.Point(334, 136);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(111, 37);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(753, 261);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.prgBar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar prgBar;
+        private System.Windows.Forms.Button btnLoad;
+    }
+}
+
---
+++ ProgresBar/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgresBar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            prgBar.Value = 100;
+            if (prgBar.Value>=100)
+            {
+                MessageBox.Show("Download Complete");
+                Application.Exit();
+                
+                
+            }
+           else if(prgBar.Value <= 0)
+            {
+                MessageBox.Show("Check your İnternet Connection");
+                Application.Exit();
+            }
+        }   
+    }
+}
